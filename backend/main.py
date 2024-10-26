5bae95163013e2bd71defc417adee4070e6dba0f
Alex Vrsecky <vrsecky102@gmail.com>
2024-10-26 16:44:06 +1100

    backend fastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv("./datasets/MergedDataset.csv")
+# Using uvicorn for application auto refresh
+# Add CORS middleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # React frontend running on port 3000
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods: GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/")
+async def root():
+    # RETRIEVE DATA FOR PREDICTIONS
+    return {"Hello": "World"}
+
--- frontend/src/pages/Analytics/index.jsx
+++ frontend/src/pages/Analytics/index.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "../../components/Form";
 
 const Analytics = () => {
+
+    const [data, setData] = useState()
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/")
+        .then(response => response.json())
+        .then(json => setData(json))
+        .catch(error => console.error("Error:", error));
+        
+    }, [])
+
+    console.log(data)
+
   return (
     <div className="bg-black">
+        {/* Form for adding or removing entries from csv file. It also acts as a filter for creating graphs */}
       <div className="grid grid-cols-1 gap-2 lg:grid-cols-3 lg:gap-3 p-5">
         <Form FormName={"Rooms"} type={"number"} />
         <Form FormName={"Distance"} type={"number"} />
@@ -20,12 +33,16 @@ const Analytics = () => {
         <a
           class="m-5 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
           href="#"
-          onClick={()  => {}}
+          onClick={() => {}}
         >
           Submit
         </a>
       </div>
-      <div>DATA VISUALISATION</div>
+      <div>
+        {/* ADD A FORM TO ADD DATA TO THE CSV FILE -- NEEDS VALIDATION */}
+        {/* ADD SCATTERPLOT THAT SHOWS HOUSES WITH -- THE SPECIFIC ATTRIBUTES SUBMITTED IN FORM */}
+        {/* SHOW A HEAT MAP WITH THE UPDATED INPUTS */}
+        </div>
     </div>
   );
 };
